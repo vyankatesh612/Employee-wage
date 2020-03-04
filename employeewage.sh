f97425c3a6c33bd66e21d2d53444f30b13839e56
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 16:45:30 +0530

    ADDED : attendence

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
 echo "Welcome to Wage computation :"
+
+function Attendence()
+{
+  if [ $((RANDOM%2)) -eq 0 ]
+  then
+    echo "Employee is present"
+  else
+    echo "Employee Absent"
+  fi
+}
+Attendence
+
