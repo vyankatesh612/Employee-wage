997e9573aa668123e2b8c02b060cb792fc431f2f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 17:11:07 +0530

    ADDED : employee daily wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to Wage computation :"
-
+WAGE_PER_HOUR=20
+FULL_HOUR=8
 function Attendence()
 {
   if [ $((RANDOM%2)) -eq 0 ]
   then
     echo "Employee is present"
+    DailyWage=$(($WAGE_PER_HOUR * $FULL_HOUR))
+    echo $DailyWage
   else
     echo "Employee Absent"
   fi
 }
+
+
+
 Attendence
 
