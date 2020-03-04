853ac00bc1cfce544563b34ef6251c099873e284
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 17:51:07 +0530

    ADDED : fulltime and parttime wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,20 +1,35 @@
+
 #!/bin/bash -x
 echo "Welcome to Wage computation :"
 WAGE_PER_HOUR=20
-FULL_HOUR=8
-function Attendence()
+
+isPartTime=1
+isFullTime=2
+
+function getEmpWage()
 {
   if [ $((RANDOM%2)) -eq 0 ]
   then
-    echo "Employee is present"
-    DailyWage=$(($WAGE_PER_HOUR * $FULL_HOUR))
+    echo "Employee Absent"
+    DailyWage=0
     echo $DailyWage
   else
-    echo "Employee Absent"
+     if [ $((RANDOM%3)) -eq $isPartTime ]
+     then
+        empHour=4
+        
+      elif [ $((RANDOM%3)) -eq $isFullTime ]
+      then
+        empHour=8
+        
+      fi
+   DailyWage=$(($WAGE_PER_HOUR * $empHour))
+    echo "Employee Wage is : "$DailyWage
+
   fi
 }
 
 
 
-Attendence
+getEmpWage
 
