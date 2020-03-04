c4e6e03e3da882ce209bd54758a6cf211c5513bb
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 18:39:21 +0530

    ADDED : monthly wage is added

--- employeewage.sh
+++ employeewage.sh
@@ -2,19 +2,26 @@
 #!/bin/bash -x
 echo "Welcome to Wage computation :"
 WAGE_PER_HOUR=20
-
+WORKING_DAY=20
 isPartTime=2
 isFullTime=1
 
 function getEmpWage (){ 
-  case $((RANDOM%3)) in 
-    $isFullTime) empHour=8
-    ;;
-    $isPartTime) empHour=4
-    ;;
-  esac
-  DailyWage=$(($WAGE_PER_HOUR * $empHour))
-	echo "Employee daily wage is :" $DailyWage
+  for ((day=1;day<$WORKING_DAY;day++))
+  do
+     case $((RANDOM%3)) in 
+           $isFullTime) empHour=8
+            ;;  
+           $isPartTime) empHour=4
+            ;;
+     esac
+     DailyWage=$(($WAGE_PER_HOUR * $empHour))
+     totalWage=$(($totalWage + $DailyWage))
+
+  done
+ echo "Employee monthly  wage is :" $totalWage
+
 }
+
 getEmpWage
 
