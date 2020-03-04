76ab7548feb99ef8d1c0d434ef80e372c8698c4c
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 18:21:26 +0530

    ADDED : employee wage using case

--- employeewage.sh
+++ employeewage.sh
@@ -3,33 +3,18 @@
 echo "Welcome to Wage computation :"
 WAGE_PER_HOUR=20
 
-isPartTime=1
-isFullTime=2
+isPartTime=2
+isFullTime=1
 
-function getEmpWage()
-{
-  if [ $((RANDOM%2)) -eq 0 ]
-  then
-    echo "Employee Absent"
-    DailyWage=0
-    echo $DailyWage
-  else
-     if [ $((RANDOM%3)) -eq $isPartTime ]
-     then
-        empHour=4
-        
-      elif [ $((RANDOM%3)) -eq $isFullTime ]
-      then
-        empHour=8
-        
-      fi
-   DailyWage=$(($WAGE_PER_HOUR * $empHour))
-    echo "Employee Wage is : "$DailyWage
-
-  fi
+function getEmpWage (){ 
+  case $((RANDOM%3)) in 
+    $isFullTime) empHour=8
+    ;;
+    $isPartTime) empHour=4
+    ;;
+  esac
+  DailyWage=$(($WAGE_PER_HOUR * $empHour))
+	echo "Employee daily wage is :" $DailyWage
 }
-
-
-
 getEmpWage
 
