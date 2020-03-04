2b781872d3c9def6ca36f726600410e7924af102
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-04 19:19:08 +0530

    ADDED : empWage till condition of total working hours

--- employeewage.sh
+++ employeewage.sh
@@ -5,9 +5,11 @@ WAGE_PER_HOUR=20
 WORKING_DAY=20
 isPartTime=2
 isFullTime=1
-
+WORKING_HOUR=100
+day=0
+hour=0
 function getEmpWage (){ 
-  for ((day=1;day<$WORKING_DAY;day++))
+  while [[ $day -le $WORKING_DAY && $hour -le $WORKING_HOUR ]]
   do
      case $((RANDOM%3)) in 
            $isFullTime) empHour=8
@@ -17,10 +19,13 @@ function getEmpWage (){
      esac
      DailyWage=$(($WAGE_PER_HOUR * $empHour))
      totalWage=$(($totalWage + $DailyWage))
+     day=$(($day + 1))
+     hour=$(($hour + $empHour))
 
   done
- echo "Employee monthly  wage is :" $totalWage
-
+ 
+  echo "Employee monthly  wage is :" $totalWage
+  echo "days : " $day " Hours : " $hour
 }
 
 getEmpWage
